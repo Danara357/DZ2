0f754e9cdb85bbe2af23f3bd0cd8efe36baee7e5
Мой первый коммит <dsbasanova@mail.ru>
2023-03-25 18:05:14 +0300

    Задача2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task2();
+    }
+    public static void task2 () {
+        System.out.println("Задача 2");
+        var dog = 8.0;
+        System.out.println(dog);
+        dog = dog + 4;
+        System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        cat = cat + 4;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+        paper = paper + 4;
+        System.out.println(paper);
     }
 }
